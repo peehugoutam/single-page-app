5e92dc0f033f97951d12488358b0ac2accb20b53
peehugoutam <varsha.goutam@gmail.com>
2024-08-12 17:14:11 +0530

    Add user api

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+// Routes
+app.use("/api/users", require("./routes/users"));
+
+// Serve static files
+app.use(express.static("public"));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
