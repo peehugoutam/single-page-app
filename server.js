771fedb031c30457cb61921b93c0701ea1e4fbc4
peehugoutam <varsha.goutam@gmail.com>
2024-08-17 12:49:16 +0530

    change code

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-// Load the http module to create an HTTP server.
 const http = require("http");
-
-// Configure the HTTP server to respond with "Hello, World!" to all requests.
-const hostname = "127.0.0.1";
 const port = 7050;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Set the HTTP status code to 200 (OK)
-  res.setHeader("Content-Type", "text/plain"); // Set the content type to plain text
-  res.end("Hello, World!\n"); // Send the response body
-});
+const requestHandler = (req, res) => {
+  res.end("Hello World!");
+};
 
-// Make the server listen on the specified port and hostname.
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const server = http.createServer(requestHandler);
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is listening on port ${port}`);
 });
